650be9fee17c1089aaff07859db4e36bfad6efa2
Divyanxhucodes <divyanshuyadu@gmail.com>
2024-10-14 14:24:00 +0530

    Add files via upload

---
+++ to find factorial using while.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+int main() {
+ 	int a,i=1,fact=1;
+ 	printf("enter the number you want to find factoial of : ");
+ 	scanf( "%d",&a);
+ 	 while (i<=a)
+      {
+	    fact=fact*i ;
+ 	     i++;
+ 	    
+		 } 
+	
+ 	
+	 printf("%d factorial of number is : %d" ,a, fact);
+	return 0;
+ }
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 
